9ca16ff1f7f15c911cd32c899bc1ea946c23e964
Jack <jacksporter24@gmail.com>
2023-07-06 17:02:10 -0400

    Search
    items are now searchable

--- client/src/components/pages/ItemsPage.js
+++ client/src/components/pages/ItemsPage.js
@@ -1,15 +1,36 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { SearchIcon } from "@chakra-ui/icons";
 import Item from "../cards/Item";
 
 function ItemsPage() {
-  const items = useSelector((state) =>
+  const allItems = useSelector((state) =>
     [...state.items].sort((a, b) => (a.name > b.name ? 1 : -1))
   );
-
+  const [search, setSearch] = useState("");
+  const items = search
+    ? allItems.filter((i) => i.name.includes(search))
+    : allItems;
   return (
     <>
       <Heading>Items</Heading>
+      <InputGroup>
+        <Input
+          placeholder="Search for an item"
+          value={search}
+          onChange={({ target }) => setSearch(target.value)}
+        />
+        <InputRightElement>
+          <SearchIcon color="gray.300" />
+        </InputRightElement>
+      </InputGroup>
+
       {items ? items.map((i) => <Item item={i} key={i.id} verbose />) : null}
     </>
   );
